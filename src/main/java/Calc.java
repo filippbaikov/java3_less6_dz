438ef28b488a7dd1afddd1a96fb1d1afb9befdf5
filipp <zeliboba0@bk.ru>
2021-10-31 17:45:20 +0300

    domashka

--- src/main/java/Calc.java
+++
@@ -1,6 +0,0 @@
-public interface Calc {
-    int add(int a, int b);
-    int mul(int a, int b);
-    int div(int a, int b);
-
-}
--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,6 +1,40 @@
+import java.util.Arrays;
+
 public class MainApp {
     public static void main(String[] args) {
 
+        //   int [] arr1= new int[]{1, 3, 5, 6, 7, 9, 8};
+        // System.out.println(Arrays.toString(task1(arr1)));
+
+    }
+
+    public static int[] task1(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 4) {
+                return Arrays.copyOfRange(arr, i + 1, arr.length);
+            }
+
+        }
+        throw new RuntimeException("нету 4");
+
+    }
+
+    public static boolean task2(int[] arr) {
+        boolean num1 = false;
+        boolean num4 = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 1 && arr[i] != 4) {
+                return false;
+            }
+            if (arr[i] == 1) {
+                num1 = true;
+            }
+            if (arr[i] == 4) {
+                num4 = true;
+            }
+
+        }
+        return num1 && num4;
     }
 
 }
